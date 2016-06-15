d187c35195d9dd526dbbc4ddd1ea6a362b553e44
Patrick Liesveld <pliesveld@users.noreply.github.com>
2016-06-15 18:57:48 -0400

    Added e2e tests and protractor configuration

---
+++ e2e-tests/scenarios.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/* https://github.com/angular/protractor/blob/master/docs/toc.md */
+
+describe('my app', function() {
+
+
+  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
+    browser.get('index.html');
+    expect(browser.getLocationAbsUrl()).toMatch("/welcome");
+  });
+
+
+  describe('login', function() {
+
+    beforeEach(function() {
+      browser.get('index.html#/login');
+    });
+
+
+    it('should render login when user navigates to /login', function() {
+      expect(element.all(by.css('[ui-view] form label')).first().getText()).
+        toMatch(/Username/);
+    });
+
+  });
+
+});
